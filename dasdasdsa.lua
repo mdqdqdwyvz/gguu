cb3f830a5090438db90010fe214bf9f0fa59f475
mdqdqdwyvz <42772049+mdqdqdwyvz@users.noreply.github.com>
2022-07-11 20:21:33 +0700

    Update dasdasdsa.lua

--- dasdasdsa.lua
+++ dasdasdsa.lua
@@ -1,3 +1,4 @@
+LOGGED = {}
 for i, v in next, workspace[game.Players.LocalPlayer.Name]["_pets_folder"]:GetChildren() do
                         if v:IsA("Model") then
                             table.insert(LOGGED, v.Name)
@@ -6,10 +7,3 @@ for i, v in next, workspace[game.Players.LocalPlayer.Name]["_pets_folder"]:GetCh
                             end
                         end
 end
-LOGGED = {}
-getgenv().id = readfile("datetest.txt")
-getgenv().unit = "sanji" -- YOU CAN CHANGE THIS TO OTHER UNITS EX SANJI
-getgenv().hidename = true -- hides name
-getgenv().dif = "Normal" -- Normal = less hard,exp. Hard = more exp,more harder
-getgenv().act = "namek_level_1" --Lower if it is too hard
-loadstring(game:HttpGet("https://rawscripts.net/raw/RELEASE-Anime-Adventures-Lobby-3936"))()
